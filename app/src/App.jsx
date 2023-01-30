621d668ef35df192aa91231517cdd45c97e20c70
Friosn <franz.rios.nino@gmail.com>
2023-01-30 18:49:21 +0100

    Routing and pages

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,12 +1,28 @@
 import { useState } from "react";
 import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import Profile from "./pages/Profile";
+import Offers from "./pages/Offers";
+import ForgotPassword from "./pages/ForgotPassword";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <h1 className="text-2xl">Hey there!</h1>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/sign-in" element={<SignIn />} />
+          <Route path="/sign-up" element={<SignUp />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/offers" element={<Offers />} />
+          <Route path="/forgot-password" element={<ForgotPassword />} />
+        </Routes>
+      </Router>
     </>
   );
 }
