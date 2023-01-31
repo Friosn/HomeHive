216d218a2071adb9598988835900c2168a306c38
Friosn <franz.rios.nino@gmail.com>
2023-01-31 16:36:45 +0100

    Color palette in index.css

--- app/src/components/NavBar.jsx
+++ app/src/components/NavBar.jsx
@@ -9,48 +9,46 @@ const NavBar = () => {
     if (route === location.pathname) return true;
   }
   return (
-    <div className="bg-white border-b shadow-sm sticky top-0">
-      <header>
-        <nav className="flex justify-between items-center px-3 max-w-6xl max-h-64">
-          <div>
-            <img
-              src="https://res.cloudinary.com/dfrmvbvdc/image/upload/v1675175894/Business%20Projects/HomeHive/02-definitive_juggoi.png"
-              alt="logo"
-              className="max-h-14 cursor-pointer"
+    <header className="bg-white border-b shadow-sm sticky top-0">
+      <nav className="flex justify-between items-center px-3 max-w-6xl max-h-64">
+        <div>
+          <img
+            src="https://res.cloudinary.com/dfrmvbvdc/image/upload/v1675175894/Business%20Projects/HomeHive/02-definitive_juggoi.png"
+            alt="logo"
+            className="h-10 cursor-pointer"
+            onClick={() => navigate("/")}
+          />
+        </div>
+        <div>
+          <ul className="flex space-x-10">
+            <li
+              className={`cursor-pointer py-3 text-sm font-semibold text-gray-500 border-b-[3px] border-b-transparent ${
+                pathRoute("/") && "text-black border-b-orange-400"
+              }`}
               onClick={() => navigate("/")}
-            />
-          </div>
-          <div>
-            <ul className="flex space-x-10">
-              <li
-                className={`cursor-pointer py-3 text-sm font-semibold text-gray-500 border-b-[3px] border-b-transparent ${
-                  pathRoute("/") && "text-black border-b-orange-400"
-                }`}
-                onClick={() => navigate("/")}
-              >
-                Home
-              </li>
-              <li
-                className={`cursor-pointer py-3 text-sm font-semibold text-gray-500 border-b-[3px] border-b-transparent ${
-                  pathRoute("/offers") && "text-black border-b-orange-400"
-                }`}
-                onClick={() => navigate("/offers")}
-              >
-                Offers
-              </li>
-              <li
-                className={`cursor-pointer py-3 text-sm font-semibold text-gray-500 border-b-[3px] border-b-transparent ${
-                  pathRoute("/sign-in") && "text-black border-b-orange-400"
-                }`}
-                onClick={() => navigate("/sign-in")}
-              >
-                Sign in
-              </li>
-            </ul>
-          </div>
-        </nav>
-      </header>
-    </div>
+            >
+              Home
+            </li>
+            <li
+              className={`cursor-pointer py-3 text-sm font-semibold text-gray-500 border-b-[3px] border-b-transparent ${
+                pathRoute("/offers") && "text-black border-b-orange-400"
+              }`}
+              onClick={() => navigate("/offers")}
+            >
+              Offers
+            </li>
+            <li
+              className={`cursor-pointer py-3 text-sm font-semibold text-gray-500 border-b-[3px] border-b-transparent ${
+                pathRoute("/sign-in") && "text-black border-b-orange-400"
+              }`}
+              onClick={() => navigate("/sign-in")}
+            >
+              Sign in
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </header>
   );
 };
 
